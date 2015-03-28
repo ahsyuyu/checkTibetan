13295da4f5b204cb23e585ac0974204473b029ba
ahsyuyu <ahsyuyu@gmail.com>
2015-03-28 17:44:26 +0800

    completed possible roots letters (-78 suffix འ situation) )

--- checkTibetan.js
+++ checkTibetan.js
@@ -2,10 +2,11 @@ var fs=require("fs");
 var roots=JSON.parse(fs.readFileSync("./roots.json","utf8"));
 
 var vowels=["","ི","ུ","ེ","ོ"];
+var suffixes=["ག", "ང", "ད", "ན", "བ", "མ", "ར", "ལ", "ས" ];
+var sa2ndSuffixes=["གས", "ངས", "བས", "མས"];
+var da2ndSuffixes=["ནད", "རད", "ལད"];
 
-var suffixes=["","ག", "ང", "ད", "ན", "བ", "མ", "ར", "ལ", "ས" ];
-
-var getRootsWithVowels2D
+var getRootsWithVowels2D = function(){
 	var rootsWithVowels2D=roots.map(function(items){
 		var stacksWithVowels=items.map(function(item){
 			var letterWithVowels=[];
@@ -16,35 +17,68 @@ var getRootsWithVowels2D
 		})
 		return stacksWithVowels;
 	});
+}
+var rootsWithVowels2D=getRootsWithVowels2D(roots);
 
-var rootsWithVowels1D=[];
-roots.map(function(items){
-	items.map(function(item){
-		for(var j=0; j<vowels.length; j++){
-			rootsWithVowels1D.push(item+vowels[j]);
+var getRootsWithVowels1D = function(){
+	var rootsWithVowels1D=[];
+	roots.map(function(items){
+		items.map(function(item){
+			for(var j=0; j<vowels.length; j++){
+				rootsWithVowels1D.push(item+vowels[j]);
+			}
+		});
+	});
+	return rootsWithVowels1D;
+}
+var rootsWithVowels1D=getRootsWithVowels1D(roots);
+
+var getRootsWithVowelsWithSuffixes2D = function(){
+	var rootsWithVowelsWithSuffixes2D=[];
+	rootsWithVowels1D.map(function(item){
+		var out=[];
+		for(var k=0; k<suffixes.length; k++){
+			out.push(item+suffixes[k]);
+		}
+		rootsWithVowelsWithSuffixes2D.push(out);
+	});
+	return rootsWithVowelsWithSuffixes2D;
+}
+var rootsWithVowelsWithSuffixes2D=getRootsWithVowelsWithSuffixes2D();
+
+var getRootsWithVowelsWithSuffixes1D = function(){
+	var rootsWithVowelsWithSuffixes1D=[];
+	rootsWithVowels1D.map(function(item){
+		for(var m=0; m<suffixes.length; m++){
+			rootsWithVowelsWithSuffixes1D.push(item+suffixes[m]);
 		}
 	});
-});
+	return rootsWithVowelsWithSuffixes1D;
+}
+var rootsWithVowelsWithSuffixes1D=getRootsWithVowelsWithSuffixes1D();
 
-var rootsWithVowelsWithSuffixes2D=[];
-rootsWithVowels1D.map(function(item){
+var getRootsWithSa2ndSuffixes1D = function(){
 	var out=[];
-	for(var k=0; k<suffixes.length; k++){
-		out.push(item+suffixes[k]);
-	}
-	rootsWithVowelsWithSuffixes2D.push(out);
-});
+	rootsWithVowels1D.map(function(item){
+		for(var i=0; i<sa2ndSuffixes.length; i++){
+			out.push(item+sa2ndSuffixes[i]);
+		}
+	});
+	return out;
+}
+var rootsWithSa2ndSuffixes1D=getRootsWithSa2ndSuffixes1D();
 
-var rootsWithVowelsWithSuffixes1D=[];
-rootsWithVowels1D.map(function(item){
-	for(var m=0; m<suffixes.length; m++){
-		rootsWithVowelsWithSuffixes1D.push(item+suffixes[m]);
-	}
-});
+var getRootsWithDa2ndSuffixes1D = function(){
+	var out=[];
+	rootsWithVowels1D.map(function(item){
+		for(var i=0; i<da2ndSuffixes.length; i++){
+			out.push(item+da2ndSuffixes[i]);
+		}
+	});
+	return out;
+}
+var rootsWithDa2ndSuffixes1D=getRootsWithDa2ndSuffixes1D();
 
-// var res=[];
-// for(var x=0; x<rootsWithVowelsWithSuffixes1D.length; x++){
-
-// }
-
-console.log(rootsWithVowelsWithSuffixes2D);
+var possibleRootLetters=rootsWithVowels1D.concat(rootsWithVowelsWithSuffixes1D).concat(rootsWithSa2ndSuffixes1D).concat(rootsWithDa2ndSuffixes1D);
+fs.writeFileSync("possibleRootLetters.json",JSON.stringify(possibleRootLetters,""," "),"utf8");
+console.log(possibleRootLetters.length);
